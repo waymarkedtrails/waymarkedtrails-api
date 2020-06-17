3d4ba41212dc245c8831b6dfd74d00e5b73bb504
Sarah Hoffmann <lonvia@denofr.de>
2020-06-17 23:17:49 +0200

    add shield API

--- api/base.py
+++ api/base.py
@@ -9,9 +9,27 @@ from datetime import datetime as dt
 
 import sqlalchemy as sa
 
+from wmt_shields import ShieldFactory
+from wmt_shields.wmt_config import WmtConfig
+
 from api import listings, tiles
 from api.details import base as details
 import db.directive
+
+# XXX configure
+shield_factory = ShieldFactory(
+                ('.slope_symbol',
+                 '.nordic_symbol',
+                 '.image_symbol',
+                 '.cai_hiking_symbol',
+                 '.swiss_mobile',
+                 '.jel_symbol',
+                 '.kct_symbol',
+                 '.osmc_symbol',
+                 '.ref_color_symbol',
+                 '.ref_symbol',
+                 '.color_box'
+                ), WmtConfig())
 
 class StatusOutput(NamedTuple):
     server_status: str
@@ -29,13 +47,20 @@ def status(conn: db.directive.connection,
 
     return StatusOutput('OK', res)
 
+@hug.format.content_type('image/svg+xml')
+def format_as_shield(data, request=None, response=None):
+    return data.create_image('svg')
 
-@hug.get()
+@hug.get(output=format_as_shield)
 def symbols(**kwargs) -> 'SVG image of a shield':
     """ Create a route shield from a set of OSM tags. The tag list must be
         given as keyword parameters."""
-    return dict(kwargs)
-    raise hug.HTTPNotFound()
+    sym = shield_factory.create(kwargs, '', style='NAT')
+    if sym is None:
+        raise hug.HTTPNotFound()
+
+    return sym
+
 
 @hug.extend_api('/list')
 def listing_api():
