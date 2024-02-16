26cedf575c823c1160d46f277091776b11367de8
Sarah Hoffmann <lonvia@denofr.de>
2024-02-16 21:23:57 +0100

    allow multiple sites on one application

--- wmt_api/frontend.py
+++ wmt_api/frontend.py
@@ -25,22 +25,31 @@ async def api_error_handler(req, resp, exception, _):
     resp.content_type = falcon.MEDIA_JSON
 
 
-def create_app(context=None):
-    if context is None:
-        context = Context(os.environ.get('WMT_CONFIG', '') or 'hiking')
+def add_flavour(app, prefix, context):
+    APIStatus(context).add_routes(app, prefix + '/v1/status')
+    APISymbols(context).add_routes(app, prefix + '/v1/symbols')
 
+    map_type = importlib.import_module(f'wmt_api.api.{context.config.MAPTYPE}')
+
+    map_type.APIListing(context).add_routes(app, prefix + '/v1/list')
+    map_type.APITiles(context).add_routes(app, prefix + '/v1/tiles')
+    map_type.APIDetails(context).add_routes(app, prefix + '/v1/details')
+
+def create_app(context=None):
     app = falcon.asgi.App(cors_enable=True, media_type=falcon.MEDIA_JSON)
     app.add_error_handler(APIError, api_error_handler)
     if 'WMT_DEBUG' in os.environ:
         app.add_error_handler(Exception, print_traceback)
-    APIStatus(context).add_routes(app, '/v1/status')
-    APISymbols(context).add_routes(app, '/v1/symbols')
 
-    map_type = importlib.import_module(f'wmt_api.api.{context.config.MAPTYPE}')
-
-    map_type.APIListing(context).add_routes(app, '/v1/list')
-    map_type.APITiles(context).add_routes(app, '/v1/tiles')
-    map_type.APIDetails(context).add_routes(app, '/v1/details')
+    if context is not None:
+        add_flavour(app, '', context)
+    else:
+        sites = os.environ.get('WMT_CONFIG', '') or 'hiking'
+        if ',' in sites:
+            for site in sites.split(','):
+                add_flavour(app, '/' + site, Context(site))
+        else:
+            add_flavour(app, '', Context(sites))
 
     return app
 
